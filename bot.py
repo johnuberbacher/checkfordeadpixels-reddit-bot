f2839e160d3a33a2ce81fd9dfa52effbc490d990
John Uberbacher <johnuberbacher@gmail.com>
2017-08-10 09:26:18 -0400

    Small adjustments
    Instead of a list of subreddits, I'm just having the bot search through
    /r/all. We'll see where she sticks and where she gets banned. Also
    changed some of the verbiage and increased the delay to about 12 hours
    in between crawls.

--- bot.py
+++ bot.py
@@ -17,18 +17,18 @@ def bot_login():
 def run_bot(r, cached_comments):
 	print ("Scanning comments...")
 	
-	for comment in r.subreddit('techsupport+pcgaming+pcmasterrace+battlestations+nvidia+buildapc+gamingpc').comments(limit=25):
+	for comment in r.subreddit('all').comments(limit=50):
 		if "dead pixel" in comment.body and comment.id not in cached_comments and  comment.author != r.user.me():
 			print("Instance of \"dead pixel(s)\" found in comment {}.".format(comment.id))
-			comment.reply("I am a bot, *bleep, bloop.* Somebody mention dead pixels? \n\n [checkfordeadpixels.com](http://checkfordeadpixels.com) is the easiest and fatest way to check for dead pixels.\n***\n^^checkfordeadpixels ^^is ^^non ^^monitzed ^^and ^^has ^^no ^^ads, ^^it's ^^totally ^^free ^^and ^^open ^^source!")
+			comment.reply("I am a bot, *bleep, bloop.* Somebody mention dead pixels? \n\n [checkfordeadpixels.com](http://checkfordeadpixels.com) is the easiest and least automated way to check for dead pixels.\n***\n^^checkfordeadpixels ^^is ^^non ^^monitzed ^^and ^^has ^^no ^^ads, ^^it's ^^totally ^^free ^^and ^^open ^^source!")
 			print("Replied to comment ID: {}.".format(comment.id))
 			cached_comments.append(comment.id)
 			
 			with open ("cached_comments.txt", "a") as f:
 				f.write(comment.id + "\n")
 	
-	print ("Sleeping for 1 minute...")
-	time.sleep(60)
+	print ("Sleeping for 12 hours...")
+	time.sleep(43200)
 	
 def get_cached_comments():
 	if not os.path.isfile("cached_comments.txt"):
@@ -44,4 +44,4 @@ r = bot_login()
 cached_comments = get_cached_comments()
 
 while True:
-	run_bot(r, cached_comments)
+	run_bot(r, cached_comments
